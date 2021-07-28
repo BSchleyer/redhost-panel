e355b38c39e0738db674daa21a43e0d0f0033a10
timholzgreve <hi@thwmd.de>
2021-07-28 08:03:37 +0200

    Venocix Backend

--- app/functions/Venocix.php
+++ app/functions/Venocix.php
@@ -15,27 +15,43 @@ class Venocix extends Controller
         ]);
     }
 
-
+#only required for redhost
     public function getTraffic($ip, $from_date = 0, $to_date = 0)
     {
-        $url = "https://api.venocix.de/temp/traffic/".env('VENOCIX_API_TOKEN')."/".$ip."/".$from_date."/".$to_date;
-        $response = self::getClient()->get($url)->getBody();
-        return json_decode($response);
+//        $url = "https://api.venocix.de/temp/traffic/".env('VENOCIX_API_TOKEN')."/".$ip."/".$from_date."/".$to_date;
+//        $response = self::getClient()->get($url)->getBody();
+//        return json_decode($response);
     }
 
-    public function setRDNS($ip, $rdns)
+    public function setRDNS($vm_id, $ip, $rdns)
     {
-        $url = "https://api.venocix.de/temp/rdns/".env('VENOCIX_API_TOKEN')."/".$ip."/".$rdns;
-//        return $url;
-        $response = self::getClient()->post($url)->getBody();
-        return json_decode($response);
+
+        $client = new \GuzzleHttp\Client();
+        $response = $client->post([
+            ''. env('VENOCIX_API_URL').'/datacenter/server/' . $vm_id . '/rdns',
+
+            [
+                'headers' => [
+                    'Content-Type' => 'application/json',
+                    'Accept' => 'application/json',
+                    'Authorization' => 'Bearer ' . env('VENOCIX_API_KEY'),
+                ],
+
+                'json' => [
+                    'ip' => $ip,
+                    'hostname' => $rdns,
+                ],
+            ]
+        ]);
+
+        return json_decode((string)$response->getBody());
     }
 
     public function getTemplates()
     {
         $client = new \GuzzleHttp\Client();
         $response = $client->get(
-            'https://reseller.venocix.de/api/v1/datacenter/templates',
+            ''. env('VENOCIX_API_URL').'/datacenter/templates',
             [
                 'headers' => [
                     'Content-Type' => 'application/json',
@@ -47,11 +63,29 @@ class Venocix extends Controller
         return json_decode((string) $response->getBody());
     }
 
+    public function getIncident($vm_id)
+    {
+
+        $client = new \GuzzleHttp\Client();
+        $response = $client->get(
+            ''. env('VENOCIX_API_URL').'/datacenter/server/'.$vm_id.'/incidents',
+            [
+                'headers' => [
+                    'Content-Type' => 'application/json',
+                    'Accept' => 'application/json',
+                    'Authorization' => 'Bearer '.env('VENOCIX_API_KEY'),
+                ],
+            ]
+        );
+
+        return json_decode((string) $response->getBody());
+    }
+
     public function createVM($cores, $memory, $disk, $ip_count, $template)
     {
         $client = new \GuzzleHttp\Client();
         $response = $client->post(
-            'https://reseller.venocix.de/api/v1/datacenter/server',
+            ''. env('VENOCIX_API_URL').'/datacenter/server',
             [
                 'headers' => [
                     'Content-Type' => 'application/json',
@@ -74,15 +108,12 @@ class Venocix extends Controller
     {
         $client = new \GuzzleHttp\Client();
         $response = $client->get(
-            'https://reseller.venocix.de/api/v1/job/'.$job_id,
+            ''. env('VENOCIX_API_URL').'/job/'.$job_id,
             [
                 'headers' => [
                     'Content-Type' => 'application/json',
                     'Accept' => 'application/json',
                     'Authorization' => 'Bearer '.env('VENOCIX_API_KEY'),
-                ],
-                'query' => [
-                    'int'=> 'consectetur',
                 ],
             ]
         );
@@ -93,7 +124,7 @@ class Venocix extends Controller
     {
         $client = new \GuzzleHttp\Client();
         $response = $client->get(
-            'https://reseller.venocix.de/api/v1/datacenter/server/'.$vm_id.'/status',
+            ''. env('VENOCIX_API_URL').'/datacenter/server/'.$vm_id.'/status',
             [
                 'headers' => [
                     'Content-Type' => 'application/json',
@@ -109,7 +140,7 @@ class Venocix extends Controller
     {
         $client = new \GuzzleHttp\Client();
         $response = $client->post(
-            'https://reseller.venocix.de/api/v1/datacenter/server/'.$vm_id.'/delete',
+            ''. env('VENOCIX_API_URL').'/datacenter/server/'.$vm_id.'/delete',
             [
                 'headers' => [
                     'Content-Type' => 'application/json',
@@ -128,7 +159,7 @@ class Venocix extends Controller
     {
         $client = new \GuzzleHttp\Client();
         $response = $client->put(
-            'https://reseller.venocix.de/api/v1/datacenter/server/'.$vm_id.'/start',
+            ''. env('VENOCIX_API_URL').'/datacenter/server/'.$vm_id.'/start',
             [
                 'headers' => [
                     'Content-Type' => 'application/json',
@@ -144,7 +175,7 @@ class Venocix extends Controller
     {
         $client = new \GuzzleHttp\Client();
         $response = $client->put(
-            'https://reseller.venocix.de/api/v1/datacenter/server/'.$vm_id.'/shutdown',
+            ''. env('VENOCIX_API_URL').'/datacenter/server/'.$vm_id.'/shutdown',
             [
                 'headers' => [
                     'Content-Type' => 'application/json',
@@ -156,11 +187,28 @@ class Venocix extends Controller
         return json_decode((string) $response->getBody());
     }
 
+    public function hardstop($vm_id) {
+
+        $client = new \GuzzleHttp\Client();
+        $response = $client->put(
+            ''. env('VENOCIX_API_URL').'/datacenter/server/'.$vm_id.'stop',
+            [
+                'headers' => [
+                    'Content-Type', 'application/json',
+                    'Accept', 'application/json',
+                    'Authorization' => 'Bearer '.env('VENOCIX_API_KEY'),
+                ],
+            ]
+        );
+
+        return json_decode((string) $response->getBody());
+    }
+
     public function reboot($vm_id)
     {
         $client = new \GuzzleHttp\Client();
         $response = $client->put(
-            'https://reseller.venocix.de/api/v1/datacenter/server/'.$vm_id.'/reboot',
+            ''. env('VENOCIX_API_URL').'/datacenter/server/'.$vm_id.'/reboot',
             [
                 'headers' => [
                     'Content-Type' => 'application/json',
@@ -176,7 +224,7 @@ class Venocix extends Controller
     {
         $client = new \GuzzleHttp\Client();
         $response = $client->put(
-            'https://reseller.venocix.de/api/v1/datacenter/server/'.$vm_id.'/password/reset',
+            ''. env('VENOCIX_API_URL').'/datacenter/server/'.$vm_id.'/password/reset',
             [
                 'headers' => [
                     'Content-Type' => 'application/json',
@@ -188,11 +236,12 @@ class Venocix extends Controller
         return json_decode((string) $response->getBody());
     }
 
-    public function reinstallVM($vm_id, $server_os = 'Debian10.0')
+    public function reinstallVM($vm_id, $serverOS="Debian10.0")
     {
+
         $client = new \GuzzleHttp\Client();
         $response = $client->put(
-            'https://reseller.venocix.de/api/v1/datacenter/server/'.$vm_id.'/reinstall',
+            ''. env('VENOCIX_API_URL').'/datacenter/server/'.$vm_id.'/reinstall',
             [
                 'headers' => [
                     'Content-Type' => 'application/json',
@@ -200,11 +249,169 @@ class Venocix extends Controller
                     'Authorization' => 'Bearer '.env('VENOCIX_API_KEY'),
                 ],
                 'json' => [
-                    'template' => $server_os,
+                    'template' => $serverOS,
                 ],
             ]
         );
         return json_decode((string) $response->getBody());
+    }
+
+    public function getVNC($vm_id)
+    {
+        $client = new \GuzzleHttp\Client();
+        $response = $client->get(
+            ''. env('VENOCIX_API_URL').'/datacenter/server/'.$vm_id.'/console',
+
+            [
+                'headers' => [
+                    'Content-Type' => 'application/json',
+                    'Accept' => 'application/json',
+                    'Authorization' => 'Bearer ' . env('VENOCIX_API_KEY'),
+                ],
+            ]
+        );
+
+        $resp = json_decode((string) $response->getBody());
+
+        return $resp->result;
+    }
+
+    public function getBackupList($vm_id)
+    {
+        $client = new \GuzzleHttp\Client();
+        $response = $client->get(
+            ''. env('VENOCIX_API_URL').'/datacenter/server/'.$vm_id.'/backups/list',
+            [
+                'headers' => [
+                    'Content-Type' => 'application/json',
+                    'Accept' => 'application/json',
+                    'Authorization' => 'Bearer ' . env('VENOCIX_API_KEY'),
+                ],
+            ]
+        );
+
+        $resp = $response->getBody();
+
+        return json_decode((string) $resp);
+
+    }
+
+    public function createBackup($vm_id)
+    {
+        $client = new \GuzzleHttp\Client();
+        $response = $client->put(
+            ''. env('VENOCIX_API_URL').'/datacenter/server/'.$vm_id.'/backups/create',
+            [
+                'headers' => [
+                    'Content-Type' => 'application/json',
+                    'Accept' => 'application/json',
+                    'Authorization' => 'Bearer ' . env('VENOCIX_API_KEY'),
+                ],
+            ]
+        );
+    }
+
+    public function getBackupStatus($vm_id)
+    {
+
+        $client = new \GuzzleHttp\Client();
+        $response = $client->get(
+            ''. env('VENOCIX_API_URL').'/datacenter/server/'.$vm_id.'/backups/status',
+            [
+                'headers' => [
+                    'Content-Type' => 'application/json',
+                    'Accept' => 'application/json',
+                    'Authorization' => 'Bearer ' . env('VENOCIX_API_KEY'),
+                ],
+            ]
+        );
+
+        $resp = $response->getBody();
+
+        return json_decode((string) $resp);
+
+    }
+
+    public function restoreBackup($vm_id, $backup_id)
+    {
+        $client = new \GuzzleHttp\Client();
+        $response = $client->post(
+            ''. env('VENOCIX_API_URL').'/datacenter/server/'.$vm_id.'/backups/restore',
+            [
+                'headers' => [
+                    'Content-Type' => 'application/json',
+                    'Accept' => 'application/json',
+                    'Authorization' => 'Bearer ' . env('VENOCIX_API_KEY'),
+                ],
+                'json' => [
+                    'backup' => $backup_id,
+                ],
+            ]
+        );
+    }
+
+    public function getSoftware()
+    {
+        $client = new \GuzzleHttp\Client();
+        $response = $client->get(
+            ''. env('VENOCIX_API_URL').'/software/list',
+            [
+                'headers' => [
+                    'Content-Type' => 'application/json',
+                    'Accept' => 'application/json',
+                    'Authorization' => 'Bearer ' . env('VENOCIX_API_KEY'),
+                ],
+            ]
+        );
+        $body = $response->getBody();
+        return(json_decode((string) $body));
+    }
+
+    public function installSoftware($vm_id, $password, $package)
+    {
+        $client = new \GuzzleHttp\Client();
+        $response = $client->post(
+            ''. env('VENOCIX_API_URL').'/software/install',
+            [
+                'headers' => [
+                    'Content-Type' => 'application/json',
+                    'Accept' => 'application/json',
+                    'Authorization' => 'Bearer ' . env('VENOCIX_API_KEY'),
+                ],
+                'json' => [
+                    'sid' => $vm_id,
+                    'package' => $package,
+                    'password' => $password,
+                    'port' => 22,
+                ],
+            ]
+        );
+        $body = $response->getBody();
+        return (json_decode((string) $body));
+    }
+
+
+    public function uninstallSoftware($vm_id, $password, $package)
+    {
+        $client = new \GuzzleHttp\Client();
+        $response = $client->post(
+            ''. env('VENOCIX_API_URL').'/software/uninstall',
+            [
+                'headers' => [
+                    'Content-Type' => 'application/json',
+                    'Accept' => 'application/json',
+                    'Authorization' => 'Bearer ' . env('VENOCIX_API_KEY'),
+                ],
+                'json' => [
+                    'sid' => $vm_id,
+                    'package' => $package,
+                    'password' => $password,
+                    'port' => 22,
+                ],
+            ]
+        );
+        $body = $response->getBody();
+        return (json_decode((string) $body));
     }
 
 }
